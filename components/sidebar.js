07464aa060217bc47136fd0103e34309d1c6680d
minforever557-netizen <minforever557@gmail.com>
2026-02-10 13:36:19 +0700

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,1 +1,16 @@
+export function sidebar(){
+
+ return `
+  <div class="sidebar">
+    <h2>Sale Support</h2>
+
+    <a href="dashboard.html">Dashboard</a>
+    <a href="#">แจ้งปัญหา</a>
+    <a href="#">Ticket</a>
+
+    <hr>
+    <a href="#">Logout</a>
+  </div>
+ `;
+}
 

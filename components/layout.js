b90524a128cb9d27c4b25128d5d80576c336bfad
minforever557-netizen <minforever557@gmail.com>
2026-02-10 13:38:00 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,1 +1,18 @@
+import { sidebar } from "./sidebar.js";
+import { navbar } from "./navbar.js";
+
+export function layout(content){
+
+ return `
+  <div class="layout">
+    ${sidebar()}
+    <div class="main">
+      ${navbar()}
+      <div class="content fade-in">
+        ${content}
+      </div>
+    </div>
+  </div>
+ `;
+}
 

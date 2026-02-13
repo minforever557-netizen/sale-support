c2f0a6c1f50478d59b7d4c98943873e3da917768
minforever557-netizen <minforever557@gmail.com>
2026-02-13 08:26:21 +0700

    Delete services directory

--- services/authService.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-export async function registerAuth(email, password){
-  const cred = await createUserWithEmailAndPassword(auth, email, password);
-  return cred.user;
-}
-
